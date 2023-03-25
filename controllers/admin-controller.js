ec420653da14220baacc0ecadb3a08652cc59136
LinTzuAn <a22341254@gmail.com>
2023-03-25 19:06:10 +0800

    feat: modify admin controller

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,17 +1,34 @@
-const { Like, Tweet, User } = require('../models')
+const { sequelize } = require('../models')
 
 const adminController = {
   getUsers: (req, res, next) => {
-    return User.findAll({
-      include: [
-        { model: User, as: 'Followers' },
-        { model: User, as: 'Followings' },
-        { model: Like },
-        { model: Tweet }
-      ]
-    })
-      .then(users => {
-        res.json({ status: 'success', users })
+    sequelize.query(
+        `SELECT 
+        Users.id, Users.email, Users.account, Users.name, Tweets.user_id, 
+        COUNT(Likes.user_id) AS liked_count, 
+        COUNT(description) AS tweet_count FROM Tweets
+        LEFT JOIN Users ON Users.id = Tweets.user_id
+        LEFT JOIN Likes ON Likes.tweet_id = Tweets.id
+       LEFT JOIN (
+          SELECT
+          Users.id, 
+          COUNT(following_id) AS following_count
+          FROM Followships
+          LEFT JOIN Users ON Users.id = Followships.follower_id
+          GROUP BY follower_id
+          ) AS x ON x.id = Tweets.user_id
+          LEFT JOIN (
+          SELECT
+          Users.id,
+          COUNT(follower_id) AS follower_count
+          FROM Followships
+          LEFT JOIN Users ON Users.id = Followships.following_id
+          GROUP BY following_id
+          ) AS y ON y.id = Tweets.user_id
+        GROUP BY Tweets.user_id, following_count, follower_count`
+    )
+      .then(data => {
+        res.status(200).json(data[0])
       })
       .catch(err => next(err))
   }
